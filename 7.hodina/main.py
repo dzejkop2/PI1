83db8aeddd84beeaded333a2e085cba2e29bfefc
dzejkop2 <91260006+dzejkop2@users.noreply.github.com>
2021-11-25 14:22:22 +0100

    7

--- 7.hodina/main.py
+++ 7.hodina/main.py
@@ -1,14 +1,17 @@
 slova = []
 a = -1
+
 subory = int(input("Zadaj koľko súborov chceš vytvoriť: "))
 
 slovo_subor = [a + 1]
-
-
 
 with open("./basnicka.txt", encoding="utf-8") as subor:
     for riadok in subor:
         slova += riadok.split()
 
+y = 0
+basen = len(slova)
 for i in range(subory):
-    open("%s.txt" % i, "w").write(slova[i])
+    if i >= basen:
+        y = i - basen
+    open("%s.txt" % i, "w").write(slova[y])
